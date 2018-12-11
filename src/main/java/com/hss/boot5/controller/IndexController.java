72fa8982c366b94b2ad2bfdf03d643fb457fb7ab
acerhss <acerhss@dbgs.com>
2018-12-11 16:12:06 +0800

    第二十五章：SpringBoot添加支持CORS跨域访问

---
+++ src/main/java/com/hss/boot5/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.hss.boot5.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/cors")
+    public String corsIndex() {
+        return "this is cirs info";
+    }
+}
